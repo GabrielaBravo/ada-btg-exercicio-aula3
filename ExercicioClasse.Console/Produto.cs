e99d5035c8f05284f8f69c0f0c7a4030627f5f7b
GabrielaBravo <gabrielabravo.usp@gmail.com>
2023-08-08 20:10:09 -0300

    initial commit

---
+++ ExercicioClasse.Console/Produto.cs
@@ -0,0 +1,32 @@
+﻿namespace ExercicioClasse.Console
+{
+    internal class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidade;
+
+        public double ValorTotalEstoque()
+        {
+            return preco * quantidade;
+            
+        }
+
+        public void AdicionarProduto( int quantidadeAdicionada)
+        {
+            
+            quantidade += quantidadeAdicionada;
+            
+        }
+
+        public void RemoverProduto(int quantidadeRemovida)
+        {
+            quantidade -= quantidadeRemovida;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {nome} | Preço: R${preco:F2} | Quantidade: {quantidade} | Valor total no estoque: R${ValorTotalEstoque():F2}";
+        }
+    }
+}
